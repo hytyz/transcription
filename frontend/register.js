ef7b579d0aa6d9c4285b9e05c08342d410da2ed6
hytyz <pomelyantseva@my.bcit.ca>
2025-11-19 21:45:58 -0800

    update register.js to support auto login

--- frontend/register.js
+++ frontend/register.js
@@ -7,6 +7,7 @@ async function createUser(email, password) {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
+        credentials: 'include',
     });
     return res.json();
 }
