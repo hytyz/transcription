a1d903b2d6c548fe3f3beaae0d8cf2674cf47720
hytyz <pomelyantseva@my.bcit.ca>
2025-11-19 20:37:09 -0800

    remove unused trash

--- frontend/script.js
+++ frontend/script.js
@@ -11,7 +11,3 @@ async function checkAuth() {
   //{"payload":{"email":"admin@admin.com","iat":1763163505,"exp":1763167105}}
 }
 
-function getCookie(name) {
-  const match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
-  return match ? decodeURIComponent(match[2]) : null;
-}
