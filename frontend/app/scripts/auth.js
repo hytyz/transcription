0557505b4234b59fae7bb9af718a17df1400695c
hytyz <pomelyantseva@my.bcit.ca>
2025-11-20 17:08:41 -0800

    refactor to use global auth state

---
+++ frontend/app/scripts/auth.js
@@ -0,0 +1,78 @@
+import { navigateTo, setAuthState, AUTH_URL } from "../router.js";
+
+async function initAuth(authUrl = AUTH_URL) {
+    const res = await fetch(`${authUrl}/me`, { credentials: "include" });
+    return res.ok ? await res.json() : null;
+}
+
+
+async function loginUser(email, password) {
+    const res = await fetch(`${AUTH_URL}/login`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email, password }),
+        credentials: 'include',
+    });
+
+    return res.json();
+}
+
+
+async function logoutUser() {
+    const res = await fetch(`${AUTH_URL}/logout`, {
+        method: 'POST',
+        credentials: 'include',
+    });
+
+    if (res.ok) {
+        setAuthState(false)
+        navigateTo('/');
+        // window.location.reload();
+    }
+}
+
+async function login() {
+    document.getElementById('auth-form').addEventListener('submit', async (e) => {
+        // console.log("inside get element by id login form")
+        e.preventDefault();
+        const email = document.getElementById('email').value.trim();
+        const password = document.getElementById('password').value;
+        const result = await loginUser(email, password);
+        // console.log(result)
+        if (result.ok) {
+            setAuthState(true)
+            navigateTo('/dashboard');
+        }
+        else console.log(result.error);
+    });
+}
+
+async function createUser(email, password) {
+    const res = await fetch(`${AUTH_URL}/create`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email, password }),
+        credentials: 'include',
+    });
+    return res.json();
+}
+
+async function register() {
+    document.getElementById('auth-form').addEventListener('submit', async (e) => {
+        e.preventDefault();
+        const email = document.getElementById('email').value.trim();
+        const password = document.getElementById('password').value;
+        const result = await createUser(email, password);
+
+        if (result.ok) {
+            setAuthState(true);
+            navigateTo('/');
+        }
+        else console.log(result.error);
+    });
+}
+
+
+export { initAuth, loginUser, logoutUser, login, register };
+
+
