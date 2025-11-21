4b80ceb453debbc01f61d9a4b9375e2acc51c39a
hytyz <pomelyantseva@my.bcit.ca>
2025-11-20 17:46:32 -0800

    update transcription.js and transribe.js to not repeat code

---
+++ frontend/app/scripts/transcribe.js
@@ -0,0 +1,154 @@
+import { navigateTo } from "../router.js";
+
+const BASE_URL = "https://polina-gateway.fly.dev"
+const api_url = `${BASE_URL}/api`;
+let gpuURL = api_url;
+let s3URL = `${BASE_URL}/s3`;
+const WebSocketURL = "wss://pataka.tail2feabe.ts.net/ws/status";
+
+function setupTranscribe() {
+    const audioFileInput = document.getElementById("audio-file");
+    const dropButton = document.querySelector(".drop-button");
+    const uploadSection = document.querySelector(".upload-section");
+    const authNav = document.querySelector(".auth-nav");
+    const uploadCard = document.getElementById("upload-progress-card");
+    const filenameLabel = document.getElementById("upload-filename");
+    const fileSizeLabel = document.getElementById("upload-file-size");
+
+    let currentJobId = null;
+    // let pollInterval = null;
+
+    dropButton.addEventListener("click", () => { audioFileInput.click(); });
+
+    audioFileInput.addEventListener("change", () => {
+        const file = audioFileInput.files[0];
+        if (file) { startTranscription(file); }
+    });
+
+    async function startTranscription(file) {
+        const formData = new FormData();
+        formData.append("jobid", String(crypto.randomUUID()));
+        formData.append("file", file);
+
+        try {
+            dropButton.disabled = true;
+            uploadSection.style.display = "none";
+            authNav.style.display = "none";
+            uploadCard.style.display = "block";
+
+            filenameLabel.textContent = file.name || "filename";
+            sessionStorage.setItem("transcriptionFilename", file.name);
+            const sizeMB = file.size / (1024 * 1024);
+            fileSizeLabel.textContent = `${sizeMB.toFixed(1)} MB`;
+
+            const res = await fetch(`${gpuURL}/upload`, { method: "POST", body: formData, credentials: "include" });
+
+            if (!res.ok) {
+                alert("error starting transcription.");
+                window.location.reload();
+            }
+
+            const data = await res.json();
+
+            if (!data.jobid) {
+                alert("no job id returned.");
+                window.location.reload();
+            }
+
+            currentJobId = data.jobid;
+            sessionStorage.setItem("currentJobId", currentJobId);
+
+            // startPollingStatus();
+            startWebSocket(currentJobId);
+
+        } catch (err) {
+            console.error("error starting transcription:", err);
+            alert("error starting transcription.");
+            window.location.reload();
+        }
+    }
+
+    // function startPollingStatus() {
+    //   if (pollInterval) clearInterval(pollInterval);
+
+    //   pollInterval = setInterval(async () => {
+    //     try {
+    //       const res = await fetch(`${s3URL}/transcriptions/${currentJobId}`);
+    //       if (!res.ok) return;
+
+    //       // const data = await res.json();
+
+    //       else {
+    //         clearInterval(pollInterval);
+    //         pollInterval = null;
+    //         fetchTranscription();
+    //       }
+    //     } catch (err) {
+    //       console.error("polling error:", err);
+    //     }
+    //   }, 10000);
+    // }
+    function startWebSocket(jobid) {
+        const ws = new WebSocket(`${WebSocketURL}`);
+
+        ws.onopen = () => {
+            ws.send(JSON.stringify({ jobid }));
+            console.log("WS connected for job:", jobid);
+        };
+
+        ws.onmessage = async (event) => {
+            const msg = JSON.parse(event.data);
+            console.log("WS status:", msg);
+
+            setTranscriptionStatus(msg.status + "...");
+
+            if (msg.status === "completed") {
+                ws.close();
+                // fetchTranscription();
+                sessionStorage.setItem("currentJobId", jobid)
+                navigateTo("/transcription")
+            }
+
+            if (msg.status === "error") {
+                ws.close();
+                alert("Transcription failed: " + msg.error);
+            }
+        };
+
+        ws.onerror = (e) => console.error("WS error:", e);
+        ws.onclose = () => console.log("WS closed");
+    }
+
+    async function setTranscriptionStatus(status) {
+        document.getElementById("progress-status").textContent = status;
+    }
+
+    // async function fetchTranscription() {
+    //     try {
+    //         const res = await fetch(`${s3URL}/transcriptions/${currentJobId}`);
+    //         if (!res.ok) {
+    //             alert("failed to fetch transcription.");
+    //             dropButton.disabled = false;
+    //             return;
+    //         }
+    //         console.log("fetch transcription response:");
+    //         console.log(res);
+    //         // console.log(await res.text());
+    //         // const json = await res.json();
+    //         let text = await res.text();
+    //         // console.log("transcription text:");
+    //         // console.log(text);
+    //         text = text.replace(/\\n/g, "\n");
+
+    //         sessionStorage.setItem("transcriptionText", text);
+    //         sessionStorage.setItem("transcriptionJobId", currentJobId);
+    //         window.location.href = "transcription.html";
+    //     } catch (err) {
+    //         console.error("error getting transcription:", err);
+    //         alert("failed to fetch transcription.");
+    //         dropButton.disabled = false;
+    //     }
+    // }
+}
+
+export { setupTranscribe }
---
+++ frontend/app/scripts/transcription.js
@@ -0,0 +1,14 @@
+import { createCardFromTemplate, activateDownloadButtons, formatToYYYYMMDD } from "./dashboard.js";
+
+// note to self date.now() gives milliseconds since epoch
+async function setupTranscription() {
+    const root = document.getElementById("file-card-root");
+    if (!root) return;
+    let currentJobId = sessionStorage.getItem("currentJobId");
+    let currentFileName = sessionStorage.getItem("transcriptionFilename")
+    const transcriptionCard = await createCardFromTemplate(currentJobId, (Date.now()/1000), currentFileName)
+    root.appendChild(transcriptionCard)
+    activateDownloadButtons();
+}
+
+export { setupTranscription }
